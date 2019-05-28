dd6f1fac12ac531a5a1f81a015914e4140d9fc6b
Gerard Klijs <g.klijs@gmail.com>
2019-05-29 00:02:15 +0200

    update lacinia-pedestal version

--- graphql-endpoint/project.clj
+++ graphql-endpoint/project.clj
@@ -3,7 +3,7 @@
   :dependencies [[clojurewerkz/money "1.10.0"]
                  [crypto-password "0.2.1"]
                  [com.stuartsierra/component "0.4.0"]
-                 [com.walmartlabs/lacinia-pedestal "0.11.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                 [com.walmartlabs/lacinia-pedestal "0.12.0" :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [hikari-cp/hikari-cp :version]
                  [nl.openweb/topology :version]
                  [org.clojure/clojure :version]
